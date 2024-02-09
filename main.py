577761733082e046e47ba41ef49e6a4f225ee8f2
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-08 22:29:26 -0800

    refactor: code styling

--- main.py
+++ main.py
@@ -1,64 +1,68 @@
 import random
 from sklearn import svm
 
-input_data = [
-    [1, 2],
-    [2, 3],
-    [3, 1]
-]
-
-output_data = [
-    1,
-    2,
-    3,
-]
-
+input_data = []
+output_data = []
 player_choice_history = []
+MODEL_TRAINING_THRESHOLD = 3
 
 
-def get_next_choice_for_computer():
-
-    if len(player_choice_history) > 2:
-        input_data.append([player_choice_history[-3],
-                          player_choice_history[-2]])
-        output_data.append(player_choice_history[-1])
-        print("Input data: ", input_data)
-        print("Output data: ", output_data)
-        model = svm.SVC()
-        model.fit(input_data, output_data)
-        player_next_choice = model.predict(
+def computer_next_choice():
+    if len(player_choice_history) > MODEL_TRAINING_THRESHOLD:
+        svm_model = svm.SVC()
+        svm_model.fit(input_data, output_data)
+        next_move_prediction = svm_model.predict(
             [[player_choice_history[-2], player_choice_history[-1]]])[0]
     else:
-        player_next_choice = random.randint(1, 3)
-    print("Predicting the human player's next choice: ", player_next_choice)
+        next_move_prediction = random.randint(1, 3)
 
-    if player_next_choice == 1:
+    print("Predicting the human player's next choice: ", next_move_prediction)
+
+    if next_move_prediction == 1:
         return 2
-    elif player_next_choice == 2:
+    elif next_move_prediction == 2:
         return 3
-    else:
-        return 1
 
-    # return random.randint(1, 3)
+    # By default, return 1
+    return 1
+
+
+'''
+Formats model training data based on player choice history in this format:
+input_data = [[1, 2], [2, 3], [3, 1], [1, 3], [3, 2], [2, 1]]
+output_data = [2, 3, 1, 3, 2, 1]
+'''
+def format_training_data(data):
+    CHOICE_MINIMUM = 3
+
+    if len(data) >= CHOICE_MINIMUM:
+        input_data.append([data[-3], data[-2]])
+        output_data.append(data[-1])
 
 
 # Main game loop
 while True:
     player_choice = int(
         input("Please make a choice (1: Rock, 2: Paper, 3: Scissors): "))
-    computer_choice = get_next_choice_for_computer()
+    computer_choice = computer_next_choice()
     print("Computer chose:", computer_choice)
 
-    # checking the winner
+    player_choice_history.append(player_choice)
+    format_training_data(player_choice_history)
+
+    # checking winner guard clause
     if player_choice == 1 and computer_choice == 2 \
             or player_choice == 2 and computer_choice == 3 \
             or player_choice == 3 and computer_choice == 1:
         print("You lose!")
+        continue
+
+    # checking loser guard clause
     elif player_choice == 2 and computer_choice == 1 \
             or player_choice == 3 and computer_choice == 2 \
             or player_choice == 1 and computer_choice == 3:
         print("You win!")
-    else:
-        print("Tie!")
+        continue
 
-    player_choice_history.append(player_choice)
+    # default case is a tie
+    print("Tie!")
