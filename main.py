4aa51aa4e770d28dd443045908e77041385cae3d
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-08 21:39:40 -0800

    feat: pipenv and svm

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
-import random 
+import random
+from sklearn import svm
+
+input_data = [
+    [1, 2],
+    [2, 3],
+    [3, 1]
+]
+
+output_data = [
+    1,
+    2,
+    3,
+]
+
+player_choice_history = []
+
 
 def get_next_choice_for_computer():
-  # update the latest human player choice records
 
-  # re-train the model
-  # just use SVM model
+    if len(player_choice_history) > 2:
+        input_data.append([player_choice_history[-3],
+                          player_choice_history[-2]])
+        output_data.append(player_choice_history[-1])
+        print("Input data: ", input_data)
+        print("Output data: ", output_data)
+        model = svm.SVC()
+        model.fit(input_data, output_data)
+        player_next_choice = model.predict(
+            [[player_choice_history[-2], player_choice_history[-1]]])[0]
+    else:
+        player_next_choice = random.randint(1, 3)
+    print("Predicting the human player's next choice: ", player_next_choice)
 
-  # predict the next human player choice
+    if player_next_choice == 1:
+        return 2
+    elif player_next_choice == 2:
+        return 3
+    else:
+        return 1
 
-  # choose the best next choice for computer, and return it
-  return random.randint(1, 3)
+    # return random.randint(1, 3)
 
+
+# Main game loop
 while True:
-  player_choice = int(input("Please make a choice (1: Rock, 2: Paper, 3: Scissors): "))
-  computer_choice = get_next_choice_for_computer() # random.randint(1, 3) # improve the computer's strategy here
-  print("Computer chose: ", computer_choice)
-  
-  if player_choice == 1 and computer_choice == 2 \
-      or player_choice == 2 and computer_choice == 3 \
-      or player_choice == 3 and computer_choice == 1:  
-    print("You lose!")
-  elif player_choice == 2 and computer_choice == 1 \
-      or player_choice == 3 and computer_choice == 2 \
-      or player_choice == 1 and computer_choice == 3:  
-    print("You win!")
-  else:
-    print("Tie!")
-  
+    player_choice = int(
+        input("Please make a choice (1: Rock, 2: Paper, 3: Scissors): "))
+    computer_choice = get_next_choice_for_computer()
+    print("Computer chose:", computer_choice)
 
+    # checking the winner
+    if player_choice == 1 and computer_choice == 2 \
+            or player_choice == 2 and computer_choice == 3 \
+            or player_choice == 3 and computer_choice == 1:
+        print("You lose!")
+    elif player_choice == 2 and computer_choice == 1 \
+            or player_choice == 3 and computer_choice == 2 \
+            or player_choice == 1 and computer_choice == 3:
+        print("You win!")
+    else:
+        print("Tie!")
+
+    player_choice_history.append(player_choice)
